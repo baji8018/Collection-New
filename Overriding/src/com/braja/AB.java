2cfb3844650ba7dbe1d85a61392949e8edef89ec
BRAJA KISHOR TARAI <BRAJA KISHOR TARAI@LAPTOP-21M959DB>
2025-01-27 22:20:20 +0530

    first msg

---
+++ Overriding/src/com/braja/AB.java
@@ -0,0 +1,8 @@
+package com.braja;
+
+public class AB {
+	public static void main(String[] args) {
+		System.out.println("hello from Ab");
+	}
+
+}
---
+++ Overriding/src/com/braja/BC.java
@@ -0,0 +1,8 @@
+package com.braja;
+
+public class BC {
+	public static void main(String[] args) {
+		System.out.println("From Bc");
+	}
+
+}
