5f92d55505f53860694ccd07f546a18ac4e73288
BRAJA KISHOR TARAI <BRAJA KISHOR TARAI@LAPTOP-21M959DB>
2025-01-27 22:37:42 +0530

    ok

---
+++ modifier/src/modifier/A.java
@@ -0,0 +1,15 @@
+package modifier;
+
+ class A{
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
+	public static void main(String[] args) {
+		System.out.println("hii");
+		A a = new A();
+		a.m1();
+		
+	}
+
+}
---
+++ modifier/src/publicmodifier/C.java
@@ -0,0 +1,5 @@
+package publicmodifier;
+
+public class C {
+
+}
